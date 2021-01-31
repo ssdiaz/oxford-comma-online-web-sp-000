95ebb335c98c84d975987568c42290777204b111
Sam D <samidiaz3@gmail.com>
2021-01-31 19:04:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,10 @@ def oxford_comma(array)
   #array[2] =
 
    #{}"and " << array[-1]
-if array.length == 1
-elsif array.length == 2
+
+if array.length == 2
   return "#{array[0]} and #{array[1]}"
-else
+elsif array.length > 2
   # array.insert(-2, " and ")
   last_element = array.pop
   array << "and "
