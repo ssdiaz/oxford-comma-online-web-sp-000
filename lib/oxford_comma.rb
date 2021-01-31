e5099c1743e5cac6fd1948051d5e3c04b5ecd65f
Sam D <samidiaz3@gmail.com>
2021-01-31 19:11:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    last_element = array.pop
+    array << "and "                       # array.insert(-2, " and ")
+    string = array.join(", ")
+    string + last_element
+  else
+    array.join("")
+  end
 end
