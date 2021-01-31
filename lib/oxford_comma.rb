07d81a94f4860090dabdec21e4634629689a9122
Sam D <samidiaz3@gmail.com>
2021-01-31 19:11:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,8 @@ def oxford_comma(array)
   if array.length == 2
     return "#{array[0]} and #{array[1]}"
   elsif array.length > 2
-    # array.insert(-2, " and ")
     last_element = array.pop
-    array << "and "
+    array << "and "                       # array.insert(-2, " and ")
     string = array.join(", ")
     string + last_element
   else
