77dfee9c8bcf3b421e5e03140aa42f7525aba0bd
Sam D <samidiaz3@gmail.com>
2021-01-31 19:10:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,13 @@
 def oxford_comma(array)
-if array.length == 2
-  return "#{array[0]} and #{array[1]}"
-elsif array.length > 2
-  # array.insert(-2, " and ")
-  last_element = array.pop
-  array << "and "
-  string = array.join(", ")
-  string + last_element
-
-else
-  array.join("")
-end
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    # array.insert(-2, " and ")
+    last_element = array.pop
+    array << "and "
+    string = array.join(", ")
+    string + last_element
+  else
+    array.join("")
+  end
 end
