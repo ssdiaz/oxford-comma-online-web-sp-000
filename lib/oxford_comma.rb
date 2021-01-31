d91310fc7f22c77e816204ff28aadfba16bbcfcd
Sam D <samidiaz3@gmail.com>
2021-01-31 19:01:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
 
    #{}"and " << array[-1]
 if array.length = 2
-  return "#{array[0]} and #{array[1]"
+  return "#{array[0]} and #{array[1]}"
 else
   # array.insert(-2, " and ")
   last_element = array.pop
